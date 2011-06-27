d677b222afe5f7915272036d1df022d9b5826abd
jimbobdog <jimbobdog@iagile2008>
2011-06-27 19:40:09 +0100

    removed source code zip from release builder script

--- Automation/Release.cmd
+++ Automation/Release.cmd
@@ -1,4 +1,4 @@
 call CopySourceToReleaseAndBuild.cmd %1
 call CreateBinariesZip_NoMerge.cmd %1
 call CreateNuGetPackages.cmd %1
-call CopySourceToReleaseAndZip.cmd %1
+rem call CopySourceToReleaseAndZip.cmd %1
