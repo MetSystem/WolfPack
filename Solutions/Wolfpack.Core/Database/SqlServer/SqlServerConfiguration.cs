2ac2f56c71f0699a9090aef9f5e16bbe5dbf9b2d
21365 <21365@216-extrawin7.aruplab.net>
2012-05-02 13:21:30 -0600

    Added ability to specify schema name for SQL Server publisher

--- Solutions/Wolfpack.Core/Database/SqlServer/SqlServerConfiguration.cs
+++ Solutions/Wolfpack.Core/Database/SqlServer/SqlServerConfiguration.cs
@@ -4,6 +4,14 @@ namespace Wolfpack.Core.Database.SqlServer
 {
     public class SqlServerConfiguration : PluginConfigBase
     {
+        private string schemaName = "dbo";
+
         public string ConnectionString { get; set; }
+        
+        public string SchemaName
+        {
+            get { return schemaName; }
+            set { schemaName = value; }
+        }
     }
 }
