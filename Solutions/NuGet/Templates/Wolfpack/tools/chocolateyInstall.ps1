c577bf8c6968fa9282a23285986c517e588d4990
jimbobdog <jimbobdog@iagile2008>
2012-10-18 01:07:21 +0100

    Added chocolately install powershell script

---
+++ Solutions/NuGet/Templates/Wolfpack/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+try {
+
+    #Your code here...
+    Write-Host "*** WOLFPACK***" -ForegroundColor yellow
+    
+    # binaries folder files
+    $wpFolder = Join-Path $packageFolder "lib\net40"
+    $wpFiles = Get-ChildItem $wpFolder
+
+    foreach ($wpFile in $wpFiles) {
+        Move-Item $wpFile.FullName $packageFolder
+    }
+    Write-Host "Binaries moved..."
+
+    # content folder files
+    $wpFolder = Join-Path $packageFolder "content"
+    $wpFiles = Get-ChildItem $wpFolder
+
+    foreach ($wpFile in $wpFiles) {
+        Move-Item $wpFile.FullName $packageFolder
+    }
+    Write-Host "Content moved..."
+    
+    Write-Host "Cleaning up..."
+    Remove-Item (Join-Path $packageFolder "*.nupkg")
+    Remove-Item (Join-Path $packageFolder "lib") -recurse -force
+    Remove-Item (Join-Path $packageFolder "content") -recurse -force
+
+    Write-ChocolateySuccess 'Wolfpack'
+} catch {
+  Write-ChocolateyFailure 'Wolfpack' $($_.Exception.Message)
+  throw 
+}
