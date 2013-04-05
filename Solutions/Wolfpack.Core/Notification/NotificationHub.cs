4351f73f6c8307eb112637a58565d7d4f6ea460c
jimbobdog <jimbobdog@iagile2008>
2013-04-05 21:49:05 +0100

    removed NSB assembly refs from test project added new notification hub wrapper

---
+++ Solutions/Wolfpack.Core/Notification/NotificationHub.cs
@@ -0,0 +1,29 @@
+﻿using Wolfpack.Core.Interfaces;
+
+namespace Wolfpack.Core.Notification
+{
+    public static class NotificationHub
+    {
+        private static INotificationHub _instance;
+
+        public static void Initialise()
+        {
+            Container.RegisterAll<INotificationRequestFilter>();
+
+            if (!Container.IsRegistered<INotificationHub>())
+            {
+                Logger.Info("Registering default NotificationHub");
+                Container.RegisterAsSingleton<INotificationHub>(typeof(DefaultNotificationHub));
+            }
+
+            Initialise(Container.Resolve<INotificationHub>());
+        }
+
+        public static void Initialise(INotificationHub instance)
+        {
+            _instance = instance;
+            _instance.Initialise();
+        }
+
+    }
+}
