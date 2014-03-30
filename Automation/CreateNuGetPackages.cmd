2f4b284957f327e9ea152ebe90fc9574e97e5890
jimbobdog <jimbobdog@localhost>
2014-03-30 21:28:59 +0100

    updated nuget packages to target v45 framework

--- Automation/CreateNuGetPackages.cmd
+++ Automation/CreateNuGetPackages.cmd
@@ -8,59 +8,59 @@ ECHO NuGetVersion is %NuGetVersion%
 pushd ..\releases\v%1\source
 
 rem WebServices
-del NuGet\Templates\Wolfpack.WebServices\lib\net40\delete.me
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.dll NuGet\Templates\Wolfpack.WebServices\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.WebServices\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices\lib\net40
+del NuGet\Templates\Wolfpack.WebServices\lib\net45\delete.me
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.dll NuGet\Templates\Wolfpack.WebServices\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.WebServices\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices\lib\net45
 
 rem WebServices Client
-del NuGet\Templates\Wolfpack.WebServices.Client\lib\net40\delete.me
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Client.dll NuGet\Templates\Wolfpack.WebServices.Client\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Client.pdb NuGet\Templates\Wolfpack.WebServices.Client\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.dll NuGet\Templates\Wolfpack.WebServices.Client\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices.Client\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.WebServices.Client\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices.Client\lib\net40
+del NuGet\Templates\Wolfpack.WebServices.Client\lib\net45\delete.me
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Client.dll NuGet\Templates\Wolfpack.WebServices.Client\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Client.pdb NuGet\Templates\Wolfpack.WebServices.Client\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.dll NuGet\Templates\Wolfpack.WebServices.Client\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.WebServices.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices.Client\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.WebServices.Client\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.pdb NuGet\Templates\Wolfpack.WebServices.Client\lib\net45
 
 
 rem Publishers
-del NuGet\Templates\Wolfpack.Publisher\lib\net40\delete.me
-copy Wolfpack.Agent\bin\debug\magnum.dll NuGet\Templates\Wolfpack.Publisher\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.dll NuGet\Templates\Wolfpack.Publisher\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.Publisher\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.Magnum.dll NuGet\Templates\Wolfpack.Publisher\lib\net40
+del NuGet\Templates\Wolfpack.Publisher\lib\net45\delete.me
+copy Wolfpack.Agent\bin\debug\magnum.dll NuGet\Templates\Wolfpack.Publisher\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.dll NuGet\Templates\Wolfpack.Publisher\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.Publisher\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.Magnum.dll NuGet\Templates\Wolfpack.Publisher\lib\net45
 
 rem HealthCheck
-del NuGet\Templates\Wolfpack.HealthCheck\lib\net40\delete.me
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.dll NuGet\Templates\Wolfpack.HealthCheck\lib\net40
-copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.HealthCheck\lib\net40
+del NuGet\Templates\Wolfpack.HealthCheck\lib\net45\delete.me
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.dll NuGet\Templates\Wolfpack.HealthCheck\lib\net45
+copy Wolfpack.Agent\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.HealthCheck\lib\net45
 
 rem Core.Testing
-del NuGet\Templates\Wolfpack.Core.Testing\lib\net40\delete.me
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Agent.exe NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Agent.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Castle.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Castle.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Magnum.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Magnum.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Testing.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net40
-copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Testing.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net40
+del NuGet\Templates\Wolfpack.Core.Testing\lib\net45\delete.me
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Agent.exe NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Agent.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Castle.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Castle.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Magnum.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Interfaces.Magnum.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Testing.dll NuGet\Templates\Wolfpack.Core.Testing\lib\net45
+copy Wolfpack.Core.Testing\bin\debug\Wolfpack.Core.Testing.pdb NuGet\Templates\Wolfpack.Core.Testing\lib\net45
 
 
 rem Wolfpack (+Sidewinder)
 del NuGet\Templates\Wolfpack\tools\delete.me
-del NuGet\Templates\Wolfpack\lib\net40\delete.me
+del NuGet\Templates\Wolfpack\lib\net45\delete.me
 del NuGet\Templates\Wolfpack\content\config\delete.me
 del NuGet\Templates\Wolfpack\content\growl\delete.me
-copy Wolfpack.Agent\bin\debug\*.* NuGet\Templates\Wolfpack\lib\net40
-del NuGet\Templates\Wolfpack\lib\net40\*.xml
-del NuGet\Templates\Wolfpack\lib\net40\*.pdb
-move NuGet\Templates\Wolfpack\lib\net40\wolfpack.agent.exe.config NuGet\Templates\Wolfpack\content
+copy Wolfpack.Agent\bin\debug\*.* NuGet\Templates\Wolfpack\lib\net45
+del NuGet\Templates\Wolfpack\lib\net45\*.xml
+del NuGet\Templates\Wolfpack\lib\net45\*.pdb
+move NuGet\Templates\Wolfpack\lib\net45\wolfpack.agent.exe.config NuGet\Templates\Wolfpack\content
 xcopy Wolfpack.Agent\bin\debug\config\*.* NuGet\Templates\Wolfpack\content\config /Y /R /I /E
 xcopy Wolfpack.Agent\bin\debug\content\*.* NuGet\Templates\Wolfpack\content\content /Y /R /I /E
 xcopy Wolfpack.Agent\bin\debug\views\*.* NuGet\Templates\Wolfpack\content\views /Y /R /I /E
