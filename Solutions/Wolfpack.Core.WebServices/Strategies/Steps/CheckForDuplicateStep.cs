e58c85a014018be5c22aab93a62c1f8297642e13
amarek <amarek@wsgelb10.intercom.at>
2015-01-27 10:25:25 +0100

    Fixed "CheckForDuplicateStep" as it only checked for duplicates in one single repository. However, all repositories have to be taken into account. New check: Throw if notification exists in ALL configured repositories Implication: SqlPublisher (FileSystemPublisher does not exist yet) needs to check for the existence of the notification as well before adding it!

--- Solutions/Wolfpack.Core.WebServices/Strategies/Steps/CheckForDuplicateStep.cs
+++ Solutions/Wolfpack.Core.WebServices/Strategies/Steps/CheckForDuplicateStep.cs
@@ -1,4 +1,7 @@
-﻿using Wolfpack.Core.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wolfpack.Core.Interfaces;
 using Wolfpack.Core.Interfaces.Entities;
 using Wolfpack.Core.WebServices.Interfaces.Entities;
 using Wolfpack.Core.WebServices.Interfaces.Exceptions;
@@ -7,22 +10,32 @@ namespace Wolfpack.Core.WebServices.Strategies.Steps
 {
     public class CheckForDuplicateStep : StepBase<WebServiceReceiverContext>
     {
-        private readonly INotificationRepository _repository;
+        private readonly IEnumerable<INotificationRepository> _repositories;
 
-        public CheckForDuplicateStep(INotificationRepository repository)
+        public CheckForDuplicateStep(IEnumerable<INotificationRepository> repositories)
         {
-            _repository = repository;
+            _repositories = repositories;
         }
 
         public override void Execute(WebServiceReceiverContext context)
         {
-            NotificationEvent notification;
-            if (_repository.GetById(context.Notification.Id, out notification))
-            {
-                Logger.Info("Message '{0}' has already been received (on {1}), discarding it!", context.Notification.Id,
-                    notification.ReceivedOnUtc);
+            // throw duplicatemessageexception if notification already exists in ALL repositories
+            if (_repositories.All(r => ExistsIn(r, context.Notification.Id)))
                 throw new DuplicateMessageException(context.Notification.Id);
+        }
+
+        private bool ExistsIn(INotificationRepository repository, Guid notificationId)
+        {
+            NotificationEvent notification;
+            if (repository.GetById(notificationId, out notification))
+            {
+                Logger.Info("Message '{0}' has already been received (on {1}), discarding it! {2}",
+                    notificationId,
+                    notification.ReceivedOnUtc,
+                    repository.GetType().Name);
+                return true;
             }
+            return false;
         }
     }
 }
