39ae4d31f69d172e295d21fbd9133ec8051d44eb
gentledepp <gentledepp@localhost>
2015-02-17 16:37:21 +0100

    Resolving Issue https://wolfpack.codeplex.com/workitem/29 Implemented "ISqlDbContextProvider" usage and SqlRepository uses contexts strictly in a "using" statement

--- Solutions/Wolfpack.Core/Repositories/Sql/ISqlDbContext.cs
+++ Solutions/Wolfpack.Core/Repositories/Sql/ISqlDbContext.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Data.Entity;
 using Wolfpack.Core.Interfaces.Entities;
 
 namespace Wolfpack.Core.Repositories.Sql
 {
-    public interface ISqlDbContext
+    public interface ISqlDbContext : IDisposable
     {
         IDbSet<NotificationEvent> Notifications { get; set; }
         void SaveChanges();
